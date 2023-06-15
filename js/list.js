185543d32f8b64a715defc89b187fac06051d1bf
Zhuzzaa <130728265+Zhuzzaa@users.noreply.github.com>
2023-06-15 19:31:32 +0600

    buttons and map

--- js/list.js
+++ js/list.js
@@ -3,3 +3,33 @@ $(document).ready(function(){
     $("nav").toggleClass("active");
   });
 });
+
+function generatePromoCode() {
+  var promoCodeElement = document.getElementById("promoCode");
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  var promoCode = "";
+  var codeLength = 8;
+
+  for (var i = 0; i < codeLength; i++) {
+    var randomIndex = Math.floor(Math.random() * characters.length);
+    promoCode += characters.charAt(randomIndex);
+  }
+
+  promoCodeElement.textContent = promoCode;
+}
+function copyPromoCode() {
+  var promoCodeElement = document.getElementById("promoCode");
+  var promoCode = promoCodeElement.textContent;
+
+  navigator.clipboard.writeText(promoCode)
+    .then(function() {
+      alert("Copied!");
+    })
+    .catch(function(error) {
+      console.error("error", error);
+    });
+}
+
+
+
+
