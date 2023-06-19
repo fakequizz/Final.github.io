0cf500c2fb03862acac68ef5cacaab0e270fd430
fakequizz <gabidenov.arsen@mail.ru>
2023-06-19 13:19:02 +0600

    update the index.js
    add the function to mute unmute music and it's gonna be similar icon

--- js/index.js
+++ js/index.js
@@ -48,39 +48,58 @@ function generatePromoCode() {
   
   
   
-var song = document.getElementsByTagName('audio')[0];
-var played = false;
-var tillPlayed = getCookie('timePlayed');
+  var song = document.getElementsByTagName('audio')[0];
+  var played = false;
+  var tillPlayed = getCookie('timePlayed');
 
-function toggleMute() {
-  if (song.muted) {
-    song.muted = false;
-    document.getElementById('muteButton').innerText = 'Mute';
-  } else {
-    song.muted = true;
-    document.getElementById('muteButton').innerText = 'Unmute';
-  }
-}
-
-function update() {
-  if (!played) {
-    if (tillPlayed) {
-      song.currentTime = tillPlayed;
-      song.play();
-      played = true;
+  function toggleMute() {
+    if (song.muted) {
+      song.muted = false;
+      document.getElementById('music-icon').classList.replace('bi-volume-mute', 'bi-volume-up');
     } else {
-      song.play();
-      played = true;
+      song.muted = true;
+      document.getElementById('music-icon').classList.replace('bi-volume-up', 'bi-volume-mute');
     }
-  } else {
-    setCookie('timePlayed', song.currentTime);
   }
-}
 
-document.addEventListener('keypress', function(event) {
-  if (event.key === 'f' || event.key === 'F') {
-    toggleMute();
+  function update() {
+    if (!played) {
+      if (tillPlayed) {
+        song.currentTime = tillPlayed;
+        song.play();
+        played = true;
+      } else {
+        song.play();
+        played = true;
+      }
+    } else {
+      setCookie('timePlayed', song.currentTime);
+    }
   }
-});
-
-setInterval(update, 1000);
+  
+  document.addEventListener('keypress', function(event) {
+    if (event.key === 'f' || event.key === 'F') {
+      toggleMute();
+    }
+  });
+  
+  setInterval(update, 1000);
+  
+  function setCookie(c_name, value, exdays) {
+    var exdate = new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
+    document.cookie = c_name + "=" + c_value;
+  }
+  
+  function getCookie(c_name) {
+    var i, x, y, ARRcookies = document.cookie.split(";");
+    for (i = 0; i < ARRcookies.length; i++) {
+      x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
+      y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
+      x = x.replace(/^\s+|\s+$/g, "");
+      if (x == c_name) {
+        return unescape(y);
+      }
+    }
+  }
